2b3d8c183ac94d32104d6303d7c738de1f58e4f2
Paschal Okafor <okaforpaschal018@gmail.com>
2022-06-17 22:18:50 +0100

    added exit svg via javascript

---
+++ exit_svg.js
@@ -0,0 +1,14 @@
+const ExitSVG = ()=>{
+    return(`
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 84.667 83" style="enable-background:new 0 0 84.667 83;" xml:space="preserve">
+    <g id="XMLID_1_">
+    
+        <line id="XMLID_57_" style="fill:none;stroke:#ffffff;stroke-width:8;stroke-linecap:round;stroke-miterlimit:10;" x1="8" y1="14.167" x2="76.667" y2="68.833"/>
+    
+        <line id="XMLID_58_" style="fill:none;stroke:#ffffff;stroke-width:8;stroke-linecap:round;stroke-miterlimit:10;" x1="76.667" y1="14.167" x2="8" y2="68.833"/>
+    </g>
+    </svg>
+
+    `)
+}
