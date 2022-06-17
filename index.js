d86f9bfbcd293aa77b03b9ffa6f41d8e6050b59a
Paschal Okafor <okaforpaschal018@gmail.com>
2022-06-17 22:43:13 +0100

    refactored index.css & created drawer functionality

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 let hamburger = document.querySelector(".hamburger")
 hamburger.innerHTML = HamburgerSVG()
+
+let drawer = document.querySelector(".drawer")
+let drawerToggleFlag = false
+
+hamburger.addEventListener("click",()=>{
+    drawerToggleFlag = !drawerToggleFlag
+
+    if(drawerToggleFlag){
+        hamburger.innerHTML = ExitSVG()
+        drawer.style.borderBottom = "1px ridge pink"
+        drawer.style.height = "160px"
+    }
+    else{
+        hamburger.innerHTML = HamburgerSVG()
+        drawer.style.borderBottom = "none"
+        drawer.style.height = "0px"
+    }
+})
+
